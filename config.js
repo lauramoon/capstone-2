cac9f04ea1a46a6ca12b2adef7a63001feb14d2f
Laura Moon <21319380+lauramoon@users.noreply.github.com>
2021-05-24 16:40:29 -0500

    update config file

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const SECRET_KEY = process.env.SECRET_KEY || "secret-inspire";
 
 const PORT = +process.env.PORT || 3001;
 
-console.log("Jobly Config:");
+console.log("Inspire Config:");
 console.log("SECRET_KEY:", SECRET_KEY);
 console.log("PORT:", PORT.toString());
 
